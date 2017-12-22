4b94e4a64e646e1a4a60774d9e4c1ffa9cdf540d
Darren Ldl <darrenldldev@gmail.com>
2017-12-22 23:03:28 +1100

    Added slices_are_equal

--- src/misc_utils.rs
+++ src/misc_utils.rs
@@ -123,3 +123,18 @@ pub fn mut_slice_to_vec_of_mut_refs<'a, T>(slice : &'a mut [T])
     }
     result
 }
+
+pub fn slices_are_equal<T>(slice1 : &[T],
+                           slice2 : &[T]) -> bool
+    where T : PartialEq
+{
+    if slice1.len() != slice2.len() {
+        return false;
+    }
+    for i in 0..slice1.len() {
+        if slice1[i] != slice2[i] {
+            return false;
+        }
+    }
+    true
+}
