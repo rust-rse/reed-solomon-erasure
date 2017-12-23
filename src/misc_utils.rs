31385a292d8836bd5ec5e633223431410a4dcfaf
Darren Ldl <darrenldldev@gmail.com>
2017-12-23 20:39:40 +1100

    Name fixes

--- src/misc_utils.rs
+++ src/misc_utils.rs
@@ -70,7 +70,7 @@ pub fn split_slice<'a, T> (slice      : &'a [T],
     result
 }
 
-pub fn break_down_slice_mut_with_index<'a, T>(slice : &'a mut [T]) -> Vec<(usize, &'a mut T)> {
+pub fn breakdown_slice_mut_with_index<'a, T>(slice : &'a mut [T]) -> Vec<(usize, &'a mut T)> {
     let mut result = Vec::with_capacity(slice.len());
     for (i, v) in split_slice_mut_with_index(slice, 1).into_iter() {
         result.push((i, &mut v[0]));
@@ -78,7 +78,7 @@ pub fn break_down_slice_mut_with_index<'a, T>(slice : &'a mut [T]) -> Vec<(usize
     result
 }
 
-pub fn break_down_slice_with_index<'a, T>(slice : &'a [T]) -> Vec<(usize, &'a T)> {
+pub fn breakdown_slice_with_index<'a, T>(slice : &'a [T]) -> Vec<(usize, &'a T)> {
     let mut result = Vec::with_capacity(slice.len());
     for (i, v) in split_slice_with_index(slice, 1).into_iter() {
         result.push((i, & v[0]));
@@ -86,7 +86,7 @@ pub fn break_down_slice_with_index<'a, T>(slice : &'a [T]) -> Vec<(usize, &'a T)
     result
 }
 
-pub fn break_down_slice_mut<'a, T>(slice : &'a mut [T]) -> Vec<&'a mut T> {
+pub fn breakdown_slice_mut<'a, T>(slice : &'a mut [T]) -> Vec<&'a mut T> {
     let mut result = Vec::with_capacity(slice.len());
     for (_, v) in split_slice_mut_with_index(slice, 1).into_iter() {
         result.push(&mut v[0]);
@@ -94,7 +94,7 @@ pub fn break_down_slice_mut<'a, T>(slice : &'a mut [T]) -> Vec<&'a mut T> {
     result
 }
 
-pub fn break_down_slice<'a, T>(slice : &'a [T]) -> Vec<&'a T> {
+pub fn breakdown_slice<'a, T>(slice : &'a [T]) -> Vec<&'a T> {
     let mut result = Vec::with_capacity(slice.len());
     for (_, v) in split_slice_with_index(slice, 1).into_iter() {
         result.push(& v[0]);
