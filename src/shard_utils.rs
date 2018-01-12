725b7b2dac6e4beae452b6463c9b1dd170f71410
Darren Ldl <darrenldldev@gmail.com>
2018-01-12 23:53:46 +0800

    Removed unused commented out code

--- src/shard_utils.rs
+++ src/shard_utils.rs
@@ -3,71 +3,12 @@
 use super::Shard;
 
 pub mod helper {
-    //use super::super::Shard;
-
     pub fn calc_offset(offset : Option<usize>) -> usize {
         match offset {
             Some(x) => x,
             None    => 0
         }
     }
-
-    /*pub fn calc_byte_count(shards     : &Vec<Shard>,
-                           byte_count : Option<usize>) -> usize {
-        let result = match byte_count {
-            Some(x) => x,
-            None    => shards[0].len()
-        };
-
-        if result == 0 { panic!("Byte count is zero"); }
-
-        result
-    }*/
-
-    /*pub fn calc_offset_and_byte_count(offset : Option<usize>,
-                                      shards : &Vec<Shard>,
-                                      byte_count : Option<usize>)
-                                      -> (usize, usize) {
-        let offset     = calc_offset(offset);
-        let byte_count = calc_byte_count(shards, byte_count);
-
-        (offset, byte_count)
-    }*/
-
-    /*pub fn calc_byte_count_option_shards(shards     : &Vec<Option<Shard>>,
-                                         byte_count : Option<usize>) -> usize {
-        let result = match byte_count {
-            Some(x) => x,
-            None    => {
-                let mut value = None;
-                for v in shards.iter() {
-                    match *v {
-                        Some(ref x) => { value = Some(x.len());
-                                         break; },
-                        None        => {},
-                    }
-                };
-                match value {
-                    Some(v) => v,
-                    None    => panic!("No shards are present")
-                }
-            }
-        };
-
-        if result == 0 { panic!("Byte count is zero"); }
-
-        result
-    }*/
-
-    /*pub fn calc_offset_and_byte_count_option_shards(offset : Option<usize>,
-                                                    shards : &Vec<Option<Shard>>,
-                                                    byte_count : Option<usize>)
-                                                    -> (usize, usize) {
-        let offset     = calc_offset(offset);
-        let byte_count = calc_byte_count_option_shards(shards, byte_count);
-
-        (offset, byte_count)
-    }*/
 }
 
 
