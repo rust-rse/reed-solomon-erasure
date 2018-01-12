c3a9c16cee95afd0e7c18ff08ec8f01203efd4e8
Darren Ldl <darrenldldev@gmail.com>
2018-01-12 23:50:42 +0800

    Removed unused commented code

--- src/lib_test.rs
+++ src/lib_test.rs
@@ -89,85 +89,11 @@ fn assert_eq_shards(s1 : &Vec<Shard>, s2 : &Vec<Shard>) {
     }
 }
 
-/*
-fn is_increasing_and_contains_data_row(indices : &Vec<usize>) -> bool {
-    let cols = indices.len();
-    for i in 0..cols-1 {
-        if indices[i] >= indices[i+1] {
-            return false
-        }
-    }
-    return indices[0] < cols
-}
-
-fn increment_indices(indices : &mut Vec<usize>,
-                     index_bound : usize) -> bool {
-    for i in (0..indices.len()).rev() {
-        indices[i] += 1;
-        if indices[i] < index_bound {
-            break;
-        }
-
-        if i == 0 {
-            return false
-        }
-
-        indices[i] = 0
-    }
-
-    return true
-}
-
-fn increment_indices_until_increasing_and_contains_data_row(indices : &mut Vec<usize>, max_index : usize) -> bool {
-    loop {
-        let valid = increment_indices(indices, max_index);
-        if !valid {
-            return false
-        }
-
-        if is_increasing_and_contains_data_row(indices) {
-            return true
-        }
-    }
-}
-
-fn find_singular_sub_matrix(m : Matrix) -> Option<Matrix> {
-    let rows = m.row_count();
-    let cols = m.col_count();
-    let mut row_indices = Vec::with_capacity(cols);
-    while increment_indices_until_increasing_and_contains_data_row(&mut row_indices, rows) {
-        let mut sub_matrix = Matrix::new(cols, cols);
-        for i in 0..row_indices.len() {
-            let r = row_indices[i];
-            for c in 0..cols {
-                sub_matrix.set(i, c, m.get(r, c));
-            }
-        }
-
-        match sub_matrix.invert() {
-            Err(matrix::Error::SingularMatrix) => return Some(sub_matrix),
-            whatever => whatever.unwrap()
-        };
-    }
-    None
-}*/
-
 fn fill_random(arr : &mut [u8]) {
     for a in arr.iter_mut() {
         *a = rand::random::<u8>();
     }
 }
-
-/*fn assert_eq_shards_with_range(shards1    : &Vec<Shard>,
-                               shards2    : &Vec<Shard>,
-                               offset     : usize,
-                               byte_count : usize) {
-    for s in 0..shards1.len() {
-        let slice1 = &shards1[s][offset..offset + byte_count];
-        let slice2 = &shards2[s][offset..offset + byte_count];
-        assert_eq!(slice1, slice2);
-    }
-}*/
 
 #[test]
 #[should_panic]
@@ -195,53 +121,6 @@ fn test_shard_count() {
         assert_eq!(total_shard_count,  r.total_shard_count());
     }
 }
-
-/*#[test]
-#[should_panic]
-fn test_calc_byte_count_byte_count_is_zero_case1() {
-    let shards = make_random_shards!(1_000, 1);
-
-    shard_utils::helper::calc_byte_count(&shards,
-                                         Some(0)); }*/
-
-/*#[test]
-#[should_panic]
-fn test_calc_byte_count_byte_count_is_zero_case2() {
-    let shards = make_random_shards!(1_000, 0);
-
-    shard_utils::helper::calc_byte_count(&shards,
-                                         None); }*/
-
-/*#[test]
-#[should_panic]
-fn test_calc_byte_count_option_shards_byte_count_is_zero_case1() {
-    let shards = make_random_shards!(1_000, 1);
-    let option_shards = shards_into_option_shards(shards);
-
-    shard_utils::helper::calc_byte_count_option_shards(&option_shards,
-                                                       Some(0)); }*/
-
-/*#[test]
-#[should_panic]
-fn test_calc_byte_count_option_shards_byte_count_is_zero_case2() {
-    let shards = make_random_shards!(1_000, 0);
-    let option_shards = shards_into_option_shards(shards);
-
-    shard_utils::helper::calc_byte_count_option_shards(&option_shards,
-                                                       None); }*/
-
-/*#[test]
-#[should_panic]
-fn test_calc_byte_count_option_shards_no_shards_present() {
-    let shards = make_random_shards!(1_000, 2);
-
-    let mut option_shards = shards_into_option_shards(shards);
-
-    option_shards[0] = None;
-    option_shards[1] = None;
-
-    shard_utils::helper::calc_byte_count_option_shards(&option_shards,
-                                                       None); }*/
 
 #[test]
 fn test_shards_into_option_shards_into_shards() {
@@ -312,8 +191,8 @@ fn test_option_shards_to_shards_too_few_shards() {
     let option_shards = shards_into_option_shards(shards);
 
     shard_utils::option_shards_to_shards(&option_shards,
-                            None,
-                            Some(11));
+                                         None,
+                                         Some(11));
 }
 
 #[test]
@@ -549,55 +428,6 @@ fn test_reconstruct_error_handling() {
         r.reconstruct(&mut shard_refs, &shards_present).unwrap();
     }
 }
-
-/*
-#[test]
-fn test_is_parity_correct() {
-    let per_shard = 33_333;
-
-    let r = ReedSolomon::new(10, 4);
-
-    let mut shards = make_random_shards!(per_shard, 14);
-
-    r.encode_parity(&mut shards, None, None);
-    assert!(r.is_parity_correct(&shards, None, None));
-
-    // corrupt shards
-    fill_random(&mut shards[5]);
-    assert!(!r.is_parity_correct(&shards, None, None));
-
-    // Re-encode
-    r.encode_parity(&mut shards, None, None);
-    fill_random(&mut shards[1]);
-    assert!(!r.is_parity_correct(&shards, None, None));
-}
-
-#[test]
-fn test_is_parity_correct_with_range() {
-    let per_shard = 33_333;
-
-    let offset = 7;
-    let byte_count = 100;
-    let op_offset = Some(offset);
-    let op_byte_count = Some(byte_count);
-
-    let r = ReedSolomon::new(10, 4);
-
-    let mut shards = make_random_shards!(per_shard, 14);
-
-    r.encode_parity(&mut shards, op_offset, op_byte_count);
-    assert!(r.is_parity_correct(&shards, op_offset, op_byte_count));
-
-    // corrupt shards
-    fill_random(&mut shards[5]);
-    assert!(!r.is_parity_correct(&shards, op_offset, op_byte_count));
-
-    // Re-encode
-    r.encode_parity(&mut shards, op_offset, op_byte_count);
-    fill_random(&mut shards[1]);
-    assert!(!r.is_parity_correct(&shards, op_offset, op_byte_count));
-}
-*/
 
 #[test]
 fn test_one_encode() {
