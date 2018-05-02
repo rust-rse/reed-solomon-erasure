c470e07e400bad7808740e2f731a705f80d841cd
Darren Ldl <darrenldldev@gmail.com>
2018-05-02 18:17:13 +1000

    Adding fuzzing code

---
+++ fuzz/fuzz_targets/fuzz_reedsolomon.rs
@@ -0,0 +1,29 @@
+#![no_main]
+#[macro_use] extern crate libfuzzer_sys;
+extern crate reed_solomon_erasure;
+
+use reed_solomon_erasure::ReedSolomon;
+
+fuzz_target!(|data: &[u8]| {
+    if data.len() >= 3 {
+        let data_shards   = data[0] as usize;
+        let parity_shards = data[1] as usize;
+        let shard_size    = data[2] as usize;
+
+        if data_shards + parity_shards <= 256
+            && data.len() >= 3 + data_shards * shard_size
+        {
+            let codec = ReedSolomon::new(data_shards, parity_shards).unwrap();
+
+            assert_eq!(codec.data_shard_count(), data_shards);
+            assert_eq!(codec.parity_shard_count(), parity_shards);
+            assert_eq!(codec.total_shard_count(), data_shards + parity_shards);
+
+            let data_slices       : Vec<&[u8]>     = data.chunks(shard_size).collect();
+            let mut parity_buffer : Vec<u8>        = vec![0u8; shard_size * parity_shards];
+            let mut parity_slices : Vec<&mut [u8]> = parity_buffer.chunks_mut(shard_size).collect();
+
+            codec.encode_sep(&data_slices, &mut parity_slices).unwrap();
+        }
+    }
+});
