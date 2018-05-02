4bb4b03221f3a20e58ccf18e2442df77ff40c49f
Darren Ldl <darrenldldev@gmail.com>
2018-05-02 19:31:51 +1000

    Added run count to fuzzing code

--- fuzz/fuzz_targets/fuzz_encode_verify.rs
+++ fuzz/fuzz_targets/fuzz_encode_verify.rs
@@ -9,8 +9,9 @@ fuzz_target!(|data: &[u8]| {
         let data_shards   = data[0] as usize;
         let parity_shards = data[1] as usize;
         let shard_size    = data[2] as usize;
+        let run_count     = data[3] as usize;
 
-        let data = &data[3..];
+        let data = &data[4..];
 
         if data_shards > 0
             && parity_shards > 0
@@ -20,30 +21,32 @@ fuzz_target!(|data: &[u8]| {
         {
             let codec = ReedSolomon::new(data_shards, parity_shards).unwrap();
 
-            assert_eq!(codec.data_shard_count(), data_shards);
-            assert_eq!(codec.parity_shard_count(), parity_shards);
-            assert_eq!(codec.total_shard_count(), data_shards + parity_shards);
+            for _ in 0..run_count {
+                assert_eq!(codec.data_shard_count(), data_shards);
+                assert_eq!(codec.parity_shard_count(), parity_shards);
+                assert_eq!(codec.total_shard_count(), data_shards + parity_shards);
 
-            let data_slices       : Vec<&[u8]>     = data.chunks(shard_size).collect();
-            let mut parity_buffer : Vec<u8>        = vec![0u8; shard_size * parity_shards];
-            {
-                let mut parity_slices : Vec<&mut [u8]> = parity_buffer.chunks_mut(shard_size).collect();
+                let data_slices       : Vec<&[u8]>     = data.chunks(shard_size).collect();
+                let mut parity_buffer : Vec<u8>        = vec![0u8; shard_size * parity_shards];
+                {
+                    let mut parity_slices : Vec<&mut [u8]> = parity_buffer.chunks_mut(shard_size).collect();
 
-                codec.encode_sep(&data_slices, &mut parity_slices).unwrap();
-            }
-
-            {
-                let parity_slices : Vec<&[u8]> = parity_buffer.chunks(shard_size).collect();
-
-                let mut slices = Vec::with_capacity(data_shards + parity_shards);
-                for &d in data_slices.iter() {
-                    slices.push(d);
-                }
-                for &p in parity_slices.iter() {
-                    slices.push(p);
+                    codec.encode_sep(&data_slices, &mut parity_slices).unwrap();
                 }
 
-                assert!(codec.verify(&slices).unwrap());
+                {
+                    let parity_slices : Vec<&[u8]> = parity_buffer.chunks(shard_size).collect();
+
+                    let mut slices = Vec::with_capacity(data_shards + parity_shards);
+                    for &d in data_slices.iter() {
+                        slices.push(d);
+                    }
+                    for &p in parity_slices.iter() {
+                        slices.push(p);
+                    }
+
+                    assert!(codec.verify(&slices).unwrap());
+                }
             }
         }
     }
