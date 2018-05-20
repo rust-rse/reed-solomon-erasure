87acc5df6ea9f8c896cb814c84a738f7605eadf0
Darren Ldl <darrenldldev@gmail.com>
2018-05-20 13:21:51 +1000

    Updated error strings

--- src/errors.rs
+++ src/errors.rs
@@ -21,19 +21,19 @@ pub enum Error {
 impl Error {
     fn to_string(&self) -> &str {
         match *self {
-            Error::TooFewShards        => "Too few shards",
-            Error::TooManyShards       => "Too many shards",
-            Error::TooFewDataShards    => "Too few data shards",
-            Error::TooManyDataShards   => "Too many data shards",
-            Error::TooFewParityShards  => "Too few parity shards",
-            Error::TooManyParityShards => "Too many parity shards",
-            Error::TooFewBufferShards  => "Too few buffer shards",
-            Error::TooManyBufferShards => "Too many buffer shards",
-            Error::IncorrectShardSize  => "Incorrect shard size",
-            Error::TooFewShardsPresent => "Too few shards present",
-            Error::EmptyShard          => "Empty shard",
-            Error::InvalidShardFlags   => "Invalid shard flags",
-            Error::InvalidIndex        => "Invalid index",
+            Error::TooFewShards        => "The number of provided shards is smaller than the one in codec",
+            Error::TooManyShards       => "The number of provided shards is greater than the one in codec",
+            Error::TooFewDataShards    => "The number of provided data shards is smaller than the one in codec",
+            Error::TooManyDataShards   => "The number of provided data shards is greater than the one in codec",
+            Error::TooFewParityShards  => "The number of provided parity shards is smaller than the one in codec",
+            Error::TooManyParityShards => "The number of provided parity shards is greater than the one in codec",
+            Error::TooFewBufferShards  => "The number of provided buffer shards is smaller than the number of parity shards in codec",
+            Error::TooManyBufferShards => "The number of provided buffer shards is greater than the number of parity shards in codec",
+            Error::IncorrectShardSize  => "At least one of the provided shards is not of the correct size",
+            Error::TooFewShardsPresent => "The number of shards present is smaller than number of parity shards, cannot reconstruct missing shards",
+            Error::EmptyShard          => "The first shard provided is of zero length",
+            Error::InvalidShardFlags   => "The number of flags does not match the total number of shards",
+            Error::InvalidIndex        => "The data shard index provided is greater or equal to the number of data shards in codec",
         }
     }
 }
--- src/errors_tests.rs
+++ src/errors_tests.rs
@@ -5,19 +5,19 @@ use errors::SBSError;
 
 #[test]
 fn test_error_to_string_is_okay() {
-    assert_eq!(Error::TooFewShards.to_string(),        "Too few shards");
-    assert_eq!(Error::TooManyShards.to_string(),       "Too many shards");
-    assert_eq!(Error::TooFewDataShards.to_string(),    "Too few data shards");
-    assert_eq!(Error::TooManyDataShards.to_string(),   "Too many data shards");
-    assert_eq!(Error::TooFewParityShards.to_string(),  "Too few parity shards");
-    assert_eq!(Error::TooManyParityShards.to_string(), "Too many parity shards");
-    assert_eq!(Error::TooFewBufferShards.to_string(),  "Too few buffer shards");
-    assert_eq!(Error::TooManyBufferShards.to_string(), "Too many buffer shards");
-    assert_eq!(Error::IncorrectShardSize.to_string(),  "Incorrect shard size");
-    assert_eq!(Error::TooFewShardsPresent.to_string(), "Too few shards present");
-    assert_eq!(Error::EmptyShard.to_string(),          "Empty shard");
-    assert_eq!(Error::InvalidShardFlags.to_string(),   "Invalid shard flags");
-    assert_eq!(Error::InvalidIndex.to_string(),        "Invalid index");
+    assert_eq!(Error::TooFewShards.to_string(),        "The number of provided shards is smaller than the one in codec");
+    assert_eq!(Error::TooManyShards.to_string(),       "The number of provided shards is greater than the one in codec");
+    assert_eq!(Error::TooFewDataShards.to_string(),    "The number of provided data shards is smaller than the one in codec");
+    assert_eq!(Error::TooManyDataShards.to_string(),   "The number of provided data shards is greater than the one in codec");
+    assert_eq!(Error::TooFewParityShards.to_string(),  "The number of provided parity shards is smaller than the one in codec");
+    assert_eq!(Error::TooManyParityShards.to_string(), "The number of provided parity shards is greater than the one in codec");
+    assert_eq!(Error::TooFewBufferShards.to_string(),  "The number of provided buffer shards is smaller than the number of parity shards in codec");
+    assert_eq!(Error::TooManyBufferShards.to_string(), "The number of provided buffer shards is greater than the number of parity shards in codec");
+    assert_eq!(Error::IncorrectShardSize.to_string(),  "At least one of the provided shards is not of the correct size");
+    assert_eq!(Error::TooFewShardsPresent.to_string(), "The number of shards present is smaller than number of parity shards, cannot reconstruct missing shards");
+    assert_eq!(Error::EmptyShard.to_string(),          "The first shard provided is of zero length");
+    assert_eq!(Error::InvalidShardFlags.to_string(),   "The number of flags does not match the total number of shards");
+    assert_eq!(Error::InvalidIndex.to_string(),        "The data shard index provided is greater or equal to the number of data shards in codec");
 }
 
 #[test]
