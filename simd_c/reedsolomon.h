4c15ef7e75680481e2ab878191a24f1e67456509
Darren Ldl <darrenldldev@gmail.com>
2019-09-24 16:26:35 +1000

    Removed nonnull

--- simd_c/reedsolomon.h
+++ simd_c/reedsolomon.h
@@ -36,7 +36,6 @@
         const size_t len
 #define PROTO(name)                     \
         PROTO_RETURN                    \
-        __attribute__((nonnull))        \
         name (PROTO_ARGS)
 
 PROTO(reedsolomon_gal_mul);
