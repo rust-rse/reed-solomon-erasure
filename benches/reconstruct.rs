86595f2344cd2b01f16304bd51ee7e027f90d322
Ayrton Sparling <ayrton@sparling.us>
2022-10-25 10:18:06 -0700

    Allow benchmarks on rust stable
    Signed-off-by: Ayrton Sparling <ayrton@sparling.us>

--- benches/reconstruct.rs
+++ benches/reconstruct.rs
@@ -1,17 +1,22 @@
-#![feature(test)]
+#![cfg_attr(nightly, feature(test))]
 
+#[cfg(nightly)]
 extern crate test;
 
+#[cfg(nightly)]
 use {
     rand::{prelude::*, Rng},
     reed_solomon_erasure::galois_8::Field,
     test::Bencher,
 };
 
+#[cfg(nightly)]
 type ReedSolomon = reed_solomon_erasure::ReedSolomon<Field>;
 
+#[cfg(nightly)]
 const SHARD_SIZE: usize = 1024;
 
+#[cfg(nightly)]
 fn run_reconstruct_bench(bencher: &mut Bencher, num_data_shards: usize, num_parity_shards: usize) {
     let mut rng = rand::thread_rng();
     let mut shards = vec![vec![0u8; SHARD_SIZE]; num_data_shards + num_parity_shards];
@@ -32,76 +37,91 @@ fn run_reconstruct_bench(bencher: &mut Bencher, num_data_shards: usize, num_pari
     });
 }
 
+#[cfg(nightly)]
 #[bench]
 fn bench_reconstruct_2_2(bencher: &mut Bencher) {
     run_reconstruct_bench(bencher, 2, 2)
 }
 
+#[cfg(nightly)]
 #[bench]
 fn bench_reconstruct_4_2(bencher: &mut Bencher) {
     run_reconstruct_bench(bencher, 4, 2)
 }
 
+#[cfg(nightly)]
 #[bench]
 fn bench_reconstruct_4_4(bencher: &mut Bencher) {
     run_reconstruct_bench(bencher, 4, 4)
 }
 
+#[cfg(nightly)]
 #[bench]
 fn bench_reconstruct_8_2(bencher: &mut Bencher) {
     run_reconstruct_bench(bencher, 8, 2)
 }
 
+#[cfg(nightly)]
 #[bench]
 fn bench_reconstruct_8_4(bencher: &mut Bencher) {
     run_reconstruct_bench(bencher, 8, 4)
 }
 
+#[cfg(nightly)]
 #[bench]
 fn bench_reconstruct_8_8(bencher: &mut Bencher) {
     run_reconstruct_bench(bencher, 8, 8)
 }
 
+#[cfg(nightly)]
 #[bench]
 fn bench_reconstruct_16_2(bencher: &mut Bencher) {
     run_reconstruct_bench(bencher, 16, 2)
 }
 
+#[cfg(nightly)]
 #[bench]
 fn bench_reconstruct_16_4(bencher: &mut Bencher) {
     run_reconstruct_bench(bencher, 16, 4)
 }
 
+#[cfg(nightly)]
 #[bench]
 fn bench_reconstruct_16_8(bencher: &mut Bencher) {
     run_reconstruct_bench(bencher, 16, 8)
 }
 
+#[cfg(nightly)]
 #[bench]
 fn bench_reconstruct_16_16(bencher: &mut Bencher) {
     run_reconstruct_bench(bencher, 16, 16)
 }
 
+#[cfg(nightly)]
 #[bench]
 fn bench_reconstruct_32_2(bencher: &mut Bencher) {
     run_reconstruct_bench(bencher, 32, 2)
 }
 
+#[cfg(nightly)]
 #[bench]
 fn bench_reconstruct_32_4(bencher: &mut Bencher) {
     run_reconstruct_bench(bencher, 32, 4)
 }
 
+#[cfg(nightly)]
 #[bench]
 fn bench_reconstruct_32_8(bencher: &mut Bencher) {
     run_reconstruct_bench(bencher, 32, 8)
 }
 
+#[cfg(nightly)]
 #[bench]
 fn bench_reconstruct_32_16(bencher: &mut Bencher) {
     run_reconstruct_bench(bencher, 32, 16)
 }
 
+#[cfg(nightly)]
 #[bench]
 fn bench_reconstruct_32_32(bencher: &mut Bencher) {
     run_reconstruct_bench(bencher, 32, 32)
